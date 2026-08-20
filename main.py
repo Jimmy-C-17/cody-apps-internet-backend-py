fa41b1c6dac7e795314cc67a15b659d9a1534060
Jimmy-C-17 <jimmyjose.copa@uab.edu.bo>
2026-08-19 20:17:54 -0400

    deploys

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     # Durante desarrollo acepta de cualquier Frontend Angular local
-    allow_origins=["http://localhost:4200", "http://localhost:8080", "http://127.0.0.1:4200"], 
+    allow_origins=["http://localhost:4200", "http://localhost:8080", "http://127.0.0.1:4200"."https://cody-apps-internet-frontend-ng-244n.vercel.app"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
